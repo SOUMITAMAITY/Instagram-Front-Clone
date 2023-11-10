b1745e8e8e05533bf27532a25cb5d6ce8d39ba92
SOUMITA MAITY <soumitamaity996@gmail.com>
2023-11-10 11:39:49 +0530

    instagram

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/home/Home";
+import "./styles/app.css"
+import Profile from "./components/profile/Profile";
+function App() {
+  return (
+    <div className="App">
+      <Profile/>
+      {/* <Home/> */}
+     {/* <Login/> */}
+     {/* <div style={{height:"100vh",display:'flex',alignItem:'center'}}>
+     <Register/>
+     </div> */}
+     
+    </div>
+  );
+}
+
+export default App;
